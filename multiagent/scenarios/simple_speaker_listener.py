22a00b18e13b629a206a8ffc8d8319d06dd5d7b0
Robby Costales <robbycostales@gmail.com>
2021-11-28 20:44:12 -0800

    Add ability to change vocabulary size

--- multiagent/scenarios/simple_speaker_listener.py
+++ multiagent/scenarios/simple_speaker_listener.py
@@ -3,10 +3,10 @@ from multiagent.core import World, Agent, Landmark
 from multiagent.scenario import BaseScenario
 
 class Scenario(BaseScenario):
-    def make_world(self):
+    def make_world(self, dim_c=3):
         world = World()
         # set any world properties first
-        world.dim_c = 3
+        world.dim_c = dim_c
         num_landmarks = 3
         # add agents
         world.agents = [Agent() for i in range(2)]
@@ -39,7 +39,7 @@ class Scenario(BaseScenario):
         world.agents[0].goal_b = np.random.choice(world.landmarks)
         # random properties for agents
         for i, agent in enumerate(world.agents):
-            agent.color = np.array([0.25,0.25,0.25])               
+            agent.color = np.array([0.25,0.25,0.25])
         # random properties for landmarks
         world.landmarks[0].color = np.array([0.65,0.15,0.15])
         world.landmarks[1].color = np.array([0.15,0.65,0.15])
@@ -81,11 +81,11 @@ class Scenario(BaseScenario):
         for other in world.agents:
             if other is agent or (other.state.c is None): continue
             comm.append(other.state.c)
-        
+
         # speaker
         if not agent.movable:
             return np.concatenate([goal_color])
         # listener
         if agent.silent:
             return np.concatenate([agent.state.p_vel] + entity_pos + comm)
-            
+
